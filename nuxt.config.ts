b069e054a824517b84c42ad13af77e16b2a37f3d
Dimash <dinmukhamed.amirov@gmail.com>
2025-08-28 17:33:02 +0500

    chore: add deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vue from '@vitejs/plugin-vue';
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -10,6 +9,8 @@ export default defineNuxtConfig({
   },
   modules: ['@pinia/nuxt'],
   vite: {
-    plugins: [vue()],
+    define: {
+      'global.crypto': 'crypto', // shim
+    },
   },
 });
