676b3bd59d8cd22cda03d8a907fc534622c9c2c2
Amirov Dinmukhamed <95445413+Dimash95@users.noreply.github.com>
2025-09-16 11:44:00 +0500

    Dev (#9)
    * feat: add nuxt page
    
    * feat: add nuxt page

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      <h1>Petly</h1>
+    </header>
+
+    <!-- Здесь будут рендериться страницы из папки /pages -->
+    <NuxtPage />
+
+    <footer>
+      <p>© 2025 Petly</p>
+    </footer>
+  </div>
+</template>
